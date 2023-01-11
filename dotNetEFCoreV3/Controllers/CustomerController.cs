46d5368a48699b0e395077b48f38e83ee7c04ec5
yogajulianp <yogajprasutiyo@gmail.com>
2023-01-11 07:02:06 +0700

    update crud to db

---
+++ dotNetEFCoreV3/Controllers/CustomerController.cs
@@ -0,0 +1,80 @@
+﻿using dotNetEFCoreV3.DataContext;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotNetEFCoreV3.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            IEnumerable<CustomerEntity> entities = _context.customerEntities.ToList();
+            return View(entities);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int? id)
+        {
+            CustomerEntity customer = _context.customerEntities.Find(id);
+            return View(customer);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Save([Bind("CustomerName, Email, Password")] CustomerEntity request)
+        {
+            _context.customerEntities.Add(request);
+            _context.SaveChanges();
+
+            return Redirect("GetAll");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            var entity = _context.customerEntities.Find(id);
+            return View(entity);
+        }
+
+        [HttpPost]
+        public IActionResult Update([Bind("Id, CustomerName, Email, Password")] CustomerEntity request)
+        {
+            //update entity
+            _context.customerEntities.Update(request);
+
+            //commit to database
+            _context.SaveChanges();
+            return Redirect("GetAll");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            var entity = _context.customerEntities.Find(id);
+            if (entity == null)
+            {
+                return Redirect("GetAll");
+            }
+
+            //hapus dari entity
+            _context.customerEntities.Remove(entity);
+
+            //commit to database
+            _context.SaveChanges();
+            return Redirect("GetAll");
+
+        }
+    }
+}
---
+++ dotNetEFCoreV3/Controllers/OrderController.cs
@@ -0,0 +1,82 @@
+﻿using dotNetEFCoreV3.DataContext;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotNetEFCoreV3.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            IEnumerable<OrderEntity> entities = _context.OrderEntities.ToList();
+            return View(entities);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int? id)
+        {
+            OrderEntity order = _context.OrderEntities.Find(id);
+            return View(order);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Save([Bind("Count, Description, ProductId")] OrderEntity request)
+        {
+            _context.OrderEntities.Add(request);
+            _context.SaveChanges();
+
+            return Redirect("GetAll");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            var entity = _context.OrderEntities.Find(id);
+            return View(entity);
+        }
+
+        [HttpPost]
+        public IActionResult Update([Bind("Id, Count, Description, ProductId")] OrderEntity request)
+        {
+            //update entity
+            _context.OrderEntities.Update(request);
+
+            //commit to database
+            _context.SaveChanges();
+            return Redirect("GetAll");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            var entity = _context.OrderEntities.Find(id);
+            if (entity == null)
+            {
+                return Redirect("GetAll");
+            }
+            //hapus dari entity
+            _context.OrderEntities.Remove(entity);
+
+            //commit to database
+            _context.SaveChanges();
+            return Redirect("GetAll");
+
+        }
+    }
+
+   
+}
